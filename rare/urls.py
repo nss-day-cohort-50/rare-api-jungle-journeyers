99f24a4c4570d8cdb48fe4df04247cc678b5c95f
Keith Parks <keithparks20@gmail.com>
2021-11-16 15:28:52 -0600

    update

--- rare/urls.py
+++ rare/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rareapi.views.auth import login_user, register_user
+from django.conf.urls import include
+from rest_framework import routers
+
+from rareapi.views.auth import login_user, register_user
+
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
