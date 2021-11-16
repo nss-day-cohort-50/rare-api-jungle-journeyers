6ddc9d904a84973529ca8df1296d715fc236ca44
galmodovar <galmodovar1@gmail.com>
2021-11-16 13:37:35 -0600

    login to work

--- rare/urls.py
+++ rare/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
+
+from rareapi.views.auth import login_user, register_user
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
