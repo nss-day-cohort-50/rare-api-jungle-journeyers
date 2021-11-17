70499c8fc9c147e288832cef2c1041844d80085e
Keith Parks <keithparks20@gmail.com>
2021-11-17 14:34:54 -0600

    added comments views

--- rare/urls.py
+++ rare/urls.py
@@ -20,9 +20,11 @@ from django.conf.urls import include
 from rest_framework import routers
 
 from rareapi.views.auth import login_user, register_user
+from rareapi.views.comments import CommentView
 
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'comments', CommentView, 'comment')
 
 urlpatterns = [
     path('', include(router.urls)),
